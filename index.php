52174247dfd8c0449f5b9bda8783c4e6b5ea0b02
DhruvaDave <30956123+DhruvaDave@users.noreply.github.com>
2017-08-17 16:28:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+
+<a href="../driver/show_car.php">Driver </a>
+<a href="../form1/home.php">Passenger </a>
+<a href="../admin/passenger.php">Admin </a>
+<a href="../carowner/post_car.php">Car owner  </a>
+</body>
+</html>
